1ea64e622126487b355d54f2b0bed3c13975777f
Matthew Kim <matthewkim93@gmail.com>
2015-10-28 13:21:26 -0500

    Completed a basic template engine

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
+var router = require("./router.js");
+
+
 //Problem: we need a simple way to look at a user's badge count and JavaScript points from a web browser
 //Solution: use Node.js to perform the profile look-ups and server our template via HTTP.
 
 //1. Create a web server
 
-//2. Handle HTTP route GET / and POST / i.e Home
-	// if url == "/" && GET
-		//show search
-	// if url == "/" && POST
-		//redirect to /:username
-//3. Handle HTTP route GET /:username i.e. /chalkers
-	//if url == "/...."
-		//get json from treehouse
-			//on "end"
-				//show profile
-			//on "error"
-				//show error
+var http = require('http');
 
-//4. Function that handles the reading of files and merge in values
-	//read from file and get a string
-		//merge values in to string
+http.createServer(function (request, response) {
+	router.home(request, response);
+	router.user(request, response);
+}).listen(3000);
+
+console.log('Server running at http://<workspace-url>/');
+
